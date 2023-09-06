58bcb07bd29410c49e573fcbceb722faa59acd3c
annebraga <144030401+annebraga@users.noreply.github.com>
2023-09-06 10:12:37 -0300

    Commit

---
+++ dsc.cs
@@ -0,0 +1,15 @@
+namespace dsc_control_d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
